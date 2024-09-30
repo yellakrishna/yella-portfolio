9953be3328ee045401dd574056c5fd9318ce2720
yellakrishna <yellakrishna7675@gmail.com>
2024-09-30 17:08:44 +0530

    some changes commint

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./add.css";
 
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
+// import { faGithub } from "@fortawesome/free-brands-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 // import Items from '../items/Items'
 
@@ -15,33 +15,7 @@ const Home = () => {
   return (
     <>
       <div className="home-container ">
-        <div className="icons">
-          <a
-            className="text-center black "
-            href="https://github.com/yellakrishna"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-          </a>
-
-          <a
-            className="black m-4"
-            href="https://www.linkedin.com/in/yella-krishna-t-950a9924b/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-          </a>
-
-          <a className="black"
-            href="https://www.instagram.com/yourprofile"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </a>
-        </div>
+       
         <div className="heading">
           <h1 className="">Yella krishna</h1>
           <h5>______Web developer</h5>
@@ -64,3 +38,32 @@ const Home = () => {
 };
 
 export default Home;
+
+
+{/* <div className="icons">
+<a
+  className="text-center black "
+  href="https://github.com/yellakrishna"
+ 
+  
+>
+  <FontAwesomeIcon icon={faGithub} size="2x" />
+</a>
+
+<a
+  className="black m-4"
+  href="https://www.linkedin.com/in/yella-krishna-t-950a9924b/"
+  
+  
+>
+  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+</a>
+
+<a className="black"
+  href="https://www.instagram.com/yourprofile"
+  
+  
+>
+  <FontAwesomeIcon icon={faInstagram} size="2x" />
+</a>
+</div> */}
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./add.css";
-// import { faGithub } from "@fortawesome/free-brands-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const data = [
   {
